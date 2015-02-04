57d9c8e81a8c0be9c3de7bea43d17e159e4f93ef
Jean Lucas <jeanleonino@gmail.com>
2015-02-04 21:39:29 -0200

    Part 2

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-var Metalsmith = require('metalsmith');
-var markdown   = require('metalsmith-markdown');
-var templates  = require('metalsmith-templates');
+var Metalsmith  = require('metalsmith');
+var collections = require('metalsmith-collections');
+var markdown    = require('metalsmith-markdown');
+var templates   = require('metalsmith-templates');
+var permalinks  = require('metalsmith-permalinks');
+var Handlebars  = require('handlebars');
+var fs          = require('fs');
+
+Handlebars.registerPartial('header', fs.readFileSync(__dirname + '/templates/partials/header.hbt').toString());
+Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/partials/footer.hbt').toString());
 
 Metalsmith(__dirname)
+  .use(collections({
+    pages: {
+      pattern: 'content/pages/*.md'
+    },
+    posts: {
+      pattern: 'content/posts/*.md',
+      sortBy: 'date',
+      reverse: true
+    }
+  }))
+  .use(permalinks({
+    pattern: ':collection/:title'
+  }))
   .use(markdown())
   .use(templates('handlebars'))
   .destination('./build')
   .build(function(err){
-    if(err) { 
-      console.log(err)
+    if(err) {
+      console.log(err);
     }
     else {
       console.log('Build complete');
     }
-  })
+  });
 
